89d918a9fb1800a8ea7f2b990ba4caff59d2e7c7
ArantxaSaEs <arysaes@gmail.com>
2019-07-27 13:48:52 -0500

    My Django Girls app, first commit

---
+++ pythonintro.py
@@ -0,0 +1,35 @@
+Ary="Arantxa Cristina"
+Ary
+print(Ary)
+print("Hello World! me llamo" + Ary)
+if 5>3:
+	print("correcto")
+if 3<2:
+	print("nai")
+else:
+	print("3 es NO menor que 2")
+if ("Cristina" in Ary):
+	print("tambien me llamo Cristina")
+elif ("Pamela" in Ary):
+	print ("tambien me llamo Pamela")
+else:
+	print("me llamo Arantxa")
+letra="e"
+if letra in Ary:
+	print("mi nombre contiene " + letra)
+elif letra not in Ary:
+	print( "mi nombre no contiene " + letra)
+otraletra="a"
+if otraletra in Ary:
+	print("mi nombre contiene " + otraletra)
+elif otraletra not in Ary:
+	print("mi nombre no contiene " + otraletra)
+
+otro_nombre="Cristina"
+if otro_nombre in Ary:
+	print("si tengo otro nombre y es " + otro_nombre)
+elif otro_nombre not in Ary:
+	print("no me llamo " + otro_nombre)
+
+
+
